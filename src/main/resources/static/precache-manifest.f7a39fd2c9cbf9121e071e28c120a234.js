4c1a2f18b3f23a3dd8238f736347eca339278594
Dikshant <dikshantyadav19@gmail.com>
2020-05-05 00:27:00 +0530

    Added Frontend Code for deployment

---
+++ src/main/resources/static/precache-manifest.f7a39fd2c9cbf9121e071e28c120a234.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f30788f20991da57bb1a963386fd77c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd08a8d2def93aa90361",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e94d96550893c5622555",
+    "url": "/static/css/main.4f58bfae.chunk.css"
+  },
+  {
+    "revision": "fd08a8d2def93aa90361",
+    "url": "/static/js/2.2a6ef013.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2a6ef013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94d96550893c5622555",
+    "url": "/static/js/main.ced5d895.chunk.js"
+  },
+  {
+    "revision": "c1a3b17b62a6d809315f",
+    "url": "/static/js/runtime-main.6732bf1c.js"
+  }
+]);
